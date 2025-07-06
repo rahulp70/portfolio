036d0bb8dfd6f23df96ee0caf8af55fa5c923e28
Rahul Padole <103524322+rahulp70@users.noreply.github.com>
2025-07-06 20:28:10 +0530

    Update Projects.tsx
    Disabled live demo for client projects

--- client/pages/Projects.tsx
+++ client/pages/Projects.tsx
@@ -164,7 +164,7 @@ export default function Projects() {
                         Confidential
                       </Button>
                     ) : (
-                      <Button size="sm" asChild>
+                      <Button disable size="sm" asChild>
                         <a
                           href={project.live}
                           className="flex items-center gap-2"
